c52cbf77819d8e704ffe3b14c19454416e58623a
Ankush Gedam <Ankush.Gedam@citiustech.com>
2021-12-31 15:42:50 +0530

    project added

---
+++ fullstack-reactive-springboot/src/main/java/com/ct/librarymanagement/FullstackReactiveSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ct.librarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackReactiveSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackReactiveSpringbootApplication.class, args);
+	}
+
+}
---
+++ fullstack-reactive-springboot/src/test/java/com/ct/librarymanagement/FullstackReactiveSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackReactiveSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
